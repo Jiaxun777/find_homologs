39df74e5f808064434b0ea0b125b43598253b620
Jiaxun777 <jli974@gatech.edu>
2024-09-08 01:26:13 -0400

    after modify scripts

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,7 +1,7 @@
-/Users/helen/Downloads/ncbi-blast-2.16.0+/bin/blastn \
+/Users/helen/Downloads/ncbi-blast-2.16.0+/bin/tblastn \
 -query $1 \
 -subject $2 \
--task blastn-short \
--outfmt '6 pident length sseq' \
-| awk '$1==100 && $2==28 {print}' > $3
+-task tblastn \
+-outfmt '6 pident qcovs sseq' \
+| awk '$1>30 && $2>90 {print}' > $3
 wc -l $3
